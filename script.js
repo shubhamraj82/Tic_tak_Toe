4f5a7de6320d6aaa99f5354e12b8446bf6f04fe3
shubham.raj <youvalsingh40@gmail.com>
2025-05-13 00:32:26 +0530

    all logic implemented

---
+++ script.js
@@ -0,0 +1,64 @@
+console.log("Welcome to Tic Tak Toe")
+let music= new Audio("music.mp3");
+let next_turn=new Audio("ting.mp3");
+let gameOver=new Audio("gameover.mp3");
+let isgameover=false;
+let turn="X";
+
+//Function to Change turn
+const changeTurn=()=>{
+    return turn ==="X"?"0":"X";
+}
+
+//Function to check for win
+const checkWin=()=>{
+    boxtext=document.getElementsByClassName('boxtext');
+    let wins=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
+    wins.forEach(e=>{
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){
+            document.querySelector('.info').innerText= boxtext[e[0]].innerText + "won";
+            isgameover=true;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="200px";
+    }  
+  })
+
+}
+
+//game logic
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext=element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText ===''){
+             boxtext.innerText=turn; 
+            turn=changeTurn();
+        next_turn.play();   
+            checkWin();
+            if(!isgameover){
+                document.getElementsByClassName("info")[0].innerText="Turn for" + turn; 
+            }
+    }
+    })
+})
+
+//reset button
+reset.addEventListener('click',()=>{
+    let boxtexts=document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element =>{
+        element.innerText="";
+    });
+    turn ="X";
+    isgameover=false;
+     document.getElementsByClassName("info")[0].innerText="Turn for" + turn; 
+     document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="0";
+
+})
